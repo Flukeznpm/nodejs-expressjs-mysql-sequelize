77e458f930336d64fbce006a5fc423fa8dc8a800
Flukeznpm <nattapong.meun@gmail.com>
2022-04-10 19:26:45 +0700

    [ADD] Test API for get data in Database

--- src/routes/enduser/index.js
+++ src/routes/enduser/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express();
+const { db } = require("../../utils/mysql");
 
 router.get("/test", async (req, res, next) => {
 	try {
-		res.send("test");
+		db.query(`select * from team`, (err, result) => {
+			res.status(200).send(result);
+		});
 	} catch (err) {
 		next(err);
 	}
